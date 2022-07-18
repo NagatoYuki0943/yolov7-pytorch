48162ae7e1855e67a0829eb181ad72ffef5d7db9
P775TM1 <lihongtu12138@outlook.com>
2022-07-18 22:02:33 +0800

    tab

--- nets/yolo.py
+++ nets/yolo.py
@@ -179,14 +179,14 @@ class RepConv(nn.Module):
 
         bn      = nn.Identity()
         conv    = nn.Conv2d(in_channels = conv.in_channels,
-                              out_channels = conv.out_channels,
-                              kernel_size = conv.kernel_size,
-                              stride=conv.stride,
-                              padding = conv.padding,
-                              dilation = conv.dilation,
-                              groups = conv.groups,
-                              bias = True,
-                              padding_mode = conv.padding_mode)
+                            out_channels = conv.out_channels,
+                            kernel_size = conv.kernel_size,
+                            stride=conv.stride,
+                            padding = conv.padding,
+                            dilation = conv.dilation,
+                            groups = conv.groups,
+                            bias = True,
+                            padding_mode = conv.padding_mode)
 
         conv.weight = torch.nn.Parameter(weights)
         conv.bias   = torch.nn.Parameter(bias)
